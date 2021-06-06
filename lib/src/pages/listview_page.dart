408cedda967c6cadf2fcbf6ac2d23c340902e33d
Abel Herrera <ahpard97@gmail.com>
2021-06-05 22:28:13 -0500

    Agregue un pull to refresh, sirve estando al inicio de la pagina donde existe scroll y permite refrescar como cuando se presiona f5 en una pagina de internet

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -50,16 +50,32 @@ class _ListaPageState extends State<ListaPage> {
   }
 
   Widget _crearLista() {
-    return ListView.builder(
-        controller: _scrollController,
-        itemCount: _listaNumeros.length,
-        itemBuilder: (BuildContext context, int index) {
-          final imagen = _listaNumeros[index];
+    return RefreshIndicator(
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+          controller: _scrollController,
+          itemCount: _listaNumeros.length,
+          itemBuilder: (BuildContext context, int index) {
+            final imagen = _listaNumeros[index];
 
-          return FadeInImage(
-              placeholder: AssetImage('assets/jar-loading.gif'),
-              image: NetworkImage('https://picsum.photos/id/$imagen/800/600'));
-        });
+            return FadeInImage(
+                placeholder: AssetImage('assets/jar-loading.gif'),
+                image:
+                    NetworkImage('https://picsum.photos/id/$imagen/800/600'));
+          }),
+    );
+  }
+
+  Future<Null> obtenerPagina1() async {
+    final duration = new Duration(seconds: 2);
+
+    new Timer(duration, () {
+      _listaNumeros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+
+    return Future.delayed(duration);
   }
 
   void _agregar10() {
